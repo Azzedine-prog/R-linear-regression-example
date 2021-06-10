66dfa2b62ba8075cdd9ad5e48e4b498246b99a77
Azzedine-prog <aazzedinelakhdar@gmail.com>
2021-06-11 00:43:12 +0100

    lastest version

--- Devoir_iot.R
+++ Devoir_iot.R
@@ -1,8 +1,23 @@
-install.packages("caTools")
+#***************************************************************
+#*Binome : 
+#*         - AZZEDINE LAKHDAR
+#*         - YOUSSEF QAISSOUMI
+#*Projet : PLATEFORME ET TECHNOLOGIE DE L'IOT
+#*
+#* année : 2020/2021
+#***************************************************************
+#install.packages("caTools")
+#install.packages("Metrics")
 # on importe notre dataset :
 mydata = read.csv("Salary_data.csv")
 #on affiche les 10 premiers lignes de notre data 
 View(head(mydata,10))
+#on plot notre data pour mieux comprendre a propos d'elle :
+#salaire en fonction d'année passé
+x <- mydata$YearsExperience
+y <- mydata$Salary
+library(ggplot2)
+ggplot(mydata, aes(x = YearsExperience, y = Salary)) + geom_point()
 #calcul de la corelation entre nos deux variables
 cor(mydata$YearsExperience, mydata$Salary, method = c("pearson", "kendall", "spearman"))
 # on divise notre dataset en deux parties une pour le training et autre pour tester
@@ -16,11 +31,23 @@ Test_data = mydata[sample==FALSE, ]
 a <- Train_data$YearsExperience
 b <- Train_data$Salary
 modele = lm(b~a, data = Train_data)
+# information a propos de notre modele
+s <- summary(modele)
+co <- s$coefficients
+co[ , 1]
+# tracer la ligne de notre modele 
+abline(modele)
+# ploter notre modele 
+ggplot(mydata, aes(x = YearsExperience, y = Salary)) + 
+  geom_point() +
+  stat_smooth(method = "lm", col = "red")
 # Show attributes of linModel
 attributes(modele)
-#exemple de prediction
-predict(modele, data.frame(a = 2.0))
-vector1 =  c (2.0, 1.3, 1.5,2.2)
+#exemple de prediction pour une valeur
+predict(modele, data.frame(a = 3.0))
+24800.510/9536.873
+
+#calculation de rmse:
 vector2 = predict(modele, data.frame(a = Test_data$YearsExperience))
 library(Metrics)
-rmse(Test_data$Salary,vector2)
+rmse(vector2,Test_data$Salary)
